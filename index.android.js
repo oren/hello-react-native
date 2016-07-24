da27b46796fb47b3d21c4e6de3c00d9d8193b10d
Oren <orengolan@gmail.com>
2016-07-24 00:18:58 -0700

    style

--- index.android.js
+++ index.android.js
@@ -5,39 +5,32 @@
  */
 
 import React, { Component } from 'react';
-import { AppRegistry, Text, View} from 'react-native';
-
-class Blink extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {showText: true};
-
-    // Toggle the state every second
-    setInterval(() => {
-      this.setState({ showText: !this.state.showText });
-    }, 1000);
-  }
-
-  render() {
-    let display = this.state.showText ? this.props.text : ' ';
-    return (
-      <Text>{display}</Text>
-    );
-  }
-}
+import { AppRegistry, Text, View, StyleSheet } from 'react-native';
 
 class AwesomeProject extends Component {
   render() {
     return (
       <View>
-        <Blink text='I love to blink' />
-        <Blink text='Yes blinking is so great' />
-        <Blink text='Why did they ever take this out of HTML' />
-        <Blink text='Look at me look at me look at me' />
+        <Text style={styles.red}>just red</Text>
+        <Text style={styles.bigblue}>just bigblue</Text>
+        <Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
+        <Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  bigblue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
+  },
+});
+
 
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
