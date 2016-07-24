f65a6723b8cb1015062763bbbfe7dce9c83a83cd
Oren <orengolan@gmail.com>
2016-07-24 00:03:50 -0700

    props, view, image, and text

--- index.android.js
+++ index.android.js
@@ -5,12 +5,34 @@
  */
 
 import React, { Component } from 'react';
-import { AppRegistry, Text } from 'react-native';
+import { AppRegistry, Text, View, Image } from 'react-native';
+
+class Greeting extends Component {
+  render() {
+
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+
+    let foo = {width: 153, height: 110};
+
+    return (
+      <View style={{alignItems: 'center'}}>
+        <Text>Hello {this.props.name}!</Text>
+        <Image source={pic} style={foo}/>
+      </View>
+    );
+  }
+}
 
 class AwesomeProject extends Component {
   render() {
     return (
-      <Text>Hello world!</Text>
+      <View style={{alignItems: 'center'}}>
+        <Greeting name='Josh' size='45' />
+        <Greeting name='Jaina' />
+        <Greeting name='Valeera' />
+      </View>
     );
   }
 }
