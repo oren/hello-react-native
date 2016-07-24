56333017a89827b695c9b4fd4953f0149f511974
Oren <orengolan@gmail.com>
2016-07-24 00:15:48 -0700

    state - blinking text

--- index.android.js
+++ index.android.js
@@ -5,22 +5,23 @@
  */
 
 import React, { Component } from 'react';
-import { AppRegistry, Text, View, Image } from 'react-native';
+import { AppRegistry, Text, View} from 'react-native';
 
-class Greeting extends Component {
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {showText: true};
+
+    // Toggle the state every second
+    setInterval(() => {
+      this.setState({ showText: !this.state.showText });
+    }, 1000);
+  }
+
   render() {
-
-    let pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    };
-
-    let foo = {width: 153, height: 110};
-
+    let display = this.state.showText ? this.props.text : ' ';
     return (
-      <View style={{alignItems: 'center'}}>
-        <Text>Hello {this.props.name}!</Text>
-        <Image source={pic} style={foo}/>
-      </View>
+      <Text>{display}</Text>
     );
   }
 }
@@ -28,13 +29,15 @@ class Greeting extends Component {
 class AwesomeProject extends Component {
   render() {
     return (
-      <View style={{alignItems: 'center'}}>
-        <Greeting name='Josh' size='45' />
-        <Greeting name='Jaina' />
-        <Greeting name='Valeera' />
+      <View>
+        <Blink text='I love to blink' />
+        <Blink text='Yes blinking is so great' />
+        <Blink text='Why did they ever take this out of HTML' />
+        <Blink text='Look at me look at me look at me' />
       </View>
     );
   }
 }
+
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
